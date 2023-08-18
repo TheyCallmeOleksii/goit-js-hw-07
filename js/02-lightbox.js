51ed206aa4df01a27d58aba8e69d430910919602
TheyCallmeOleksii <DionodeKor@gmail.com>
2023-08-18 09:09:35 +0300

    Summary 2th task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+// import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+const newElImg = createLi(galleryItems);
+
+ulEl.insertAdjacentHTML("beforeend", newElImg);
+
+// SimpleLightbox
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionPosition: "bottom",
+  captionsData: "alt",
+});
+
+function createLi(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`;
+    })
+    .join("");
+}
